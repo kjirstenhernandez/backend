c967bd612e66c78c3f1c125bafed8f9762edb8f3
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-18 15:05:39 -0600

    fixed DB connection issue

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,11 +3,8 @@ const data = require("../db/dbConnect")
 const getInfo = async (req, res, next) => {
     console.log("controller test")
     const client = await data.dbConnection();
-    const db = client.db("testing");
-    const userList = await db.collection("users").find().toArray();
-    console.log(userList)
-    if (userList) {
-        userList.toArray().then((list) => {
+    if (client) {
+        client.toArray().then((list) => {
             res.setHeader("Content-Type", "application/json");
             res.status(200).json(list[0])
         } )
--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,17 +1,16 @@
-const MongoClient  = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb')
 const env = require("dotenv").config();
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri)
 
 const dbConnection = async () => {
     try{
-        await client.connect(uri);
-        console.log("DB connected");
+        await client.connect();
+        const databasesList = await client.db("Testing").collection("Users").find();
+        return databasesList
     } catch {
         console.error(e);
     }
 }
-
-
 
 module.exports = { dbConnection }
